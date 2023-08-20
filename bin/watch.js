fbee8d650afc73ce4d97728d063ae65043006e80
FlokiTV <dawysonw@gmail.com>
2023-08-20 14:44:40 -0300

    fix

--- bin/watch.js
+++ bin/watch.js
@@ -3,11 +3,16 @@ const crypto = require("crypto")
 const { exec } = require("child_process");
 
 const directoryToWatch = './src';
+const _DIST = __dirname + "/dist/"
 
 let buildingSvelte = false
 let buildingCSS = false
 
-const watcher = fs.watch(directoryToWatch, (eventType, filename) => {
+if (!fs.existsSync(_DIST)) {
+    fs.mkdirSync(_DIST);
+}
+
+const buidSvelte = (eventType, filename) => {
     if (!buildingSvelte && filename.endsWith(".svelte")) {
         console.log(`starting building ${filename}`)
         let s = Date.now()
@@ -18,17 +23,22 @@ const watcher = fs.watch(directoryToWatch, (eventType, filename) => {
             buildingSvelte = false
         })
     }
+}
 
+const watcher = fs.watch(directoryToWatch, buidSvelte);
+
+watcher.on('error', error => {
+    console.error(`Watcher error: ${error}`);
 });
 
 let lastHex = ""
-const cssWatcher = fs.watch(__dirname + "/dist/index.css", (eventType, filename) => {
+const cssWatcher = fs.watch(__dirname + "/dist", (eventType, filename) => {
     if (!buildingCSS && filename.endsWith(".css")) {
         let file = fs.readFileSync(__dirname + "/dist/index.css")
         let hash = crypto.createHash("sha256")
         hash.update(file)
         let hex = hash.digest("hex")
-        if(hex !== lastHex){
+        if (hex !== lastHex) {
             console.log(`starting building ${filename}`)
             lastHex = hex
             let s = Date.now()
@@ -38,15 +48,13 @@ const cssWatcher = fs.watch(__dirname + "/dist/index.css", (eventType, filename)
                 console.log("build css done " + (Date.now() - s) + "ms")
                 buildingCSS = false
             })
-        }else{
+        } else {
             console.log(`no changes on ${filename} ${eventType}`)
         }
     }
 })
 
-watcher.on('error', error => {
-    console.error(`Watcher error: ${error}`);
-});
+
 
 cssWatcher.on('error', error => {
     console.error(`Watcher error: ${error}`);
