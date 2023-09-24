715454a1c2c328dc9c8dcf083476b0f0d88655a4
FlokiTV <dawysonw@gmail.com>
2023-09-24 08:25:13 -0300

    fix: minor

--- bin/make-css.js
+++ bin/make-css.js
@@ -7,18 +7,21 @@ try {
 let style = fs.readFileSync(process.cwd() + "/dist/style.css", {
   encoding: "utf-8",
 });
+
 let textEncoder = new TextEncoder();
 let encodedString = textEncoder.encode(style);
 const css = "`" + style + "`";
+
 const content = `
 let styleSheet = document.createElement("style");
 let textDecoder = new TextDecoder();
 let enc = new Uint8Array([${encodedString.toString()}]);
 styleSheet.innerText = textDecoder.decode(enc);
 document.head.appendChild(styleSheet);`;
+
 fs.writeFile(process.cwd() + "/dist/style.js", content, (err) => {
   if (err) {
-    console.error("Ocorreu um erro ao criar o arquivo:", err);
+    console.error("Error:", err);
     return;
   }
   console.log("style.js done");
