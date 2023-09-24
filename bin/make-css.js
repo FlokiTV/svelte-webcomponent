230121ff27733a32d03aedce7ddb1e8c3cf2c5e2
FlokiTV <dawysonw@gmail.com>
2023-09-24 08:24:21 -0300

    fix: why `var` man??

--- bin/make-css.js
+++ bin/make-css.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 
 try {
-    fs.rmSync('./dist/style.js')
-} catch (error) {
+  fs.rmSync(process.cwd() + "/dist/style.js");
+} catch (error) {}
 
-}
-
-let style = fs.readFileSync("./dist/style.css", { encoding: "utf-8" })
-var textEncoder = new TextEncoder();
-var encodedString = textEncoder.encode(style);
-const css = "`" + style + "`"
+let style = fs.readFileSync(process.cwd() + "/dist/style.css", {
+  encoding: "utf-8",
+});
+let textEncoder = new TextEncoder();
+let encodedString = textEncoder.encode(style);
+const css = "`" + style + "`";
 const content = `
 let styleSheet = document.createElement("style");
 let textDecoder = new TextDecoder();
 let enc = new Uint8Array([${encodedString.toString()}]);
 styleSheet.innerText = textDecoder.decode(enc);
-document.head.appendChild(styleSheet);`
-fs.writeFile('./dist/style.js', content, (err) => {
-    if (err) {
-        console.error('Ocorreu um erro ao criar o arquivo:', err);
-        return;
-    }
-    console.log('style.js done');
+document.head.appendChild(styleSheet);`;
+fs.writeFile(process.cwd() + "/dist/style.js", content, (err) => {
+  if (err) {
+    console.error("Ocorreu um erro ao criar o arquivo:", err);
+    return;
+  }
+  console.log("style.js done");
 });
